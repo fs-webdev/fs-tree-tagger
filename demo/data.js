9bd2c2b6ce2e3ed714ff32a1d22e0263542b5f9f
Chad Eddington <cdavideddington@gmail.com>
2016-04-15 15:28:58 -0600

    First

---
+++ demo/data.js
@@ -0,0 +1,31 @@
+window.testData = [
+  {
+    "pid": "KWQQ-WCB",
+    "name": "Delilah ROBINSON",
+    "lifespan": "1817-1848"
+  },
+  {
+    "pid": "KWQM-Y2P",
+    "name": "Denia Byreleah Tharp",
+    "lifespan": "1832-1900",
+    "thumbSquareUrl": "https://integration.familysearch.org/sandbox/v2/TH-501-45908-27-54/thumb200s.jpg?ctx=ArtCtxPublic"
+  },
+  {
+    "pid": "KWHW-SMX",
+    "name": "Dusty Crophopper",
+    "lifespan": "2001-Deceased",
+    "thumbSquareUrl": "https://integration.familysearch.org/sandbox/v2/TH-501-45908-27-54/thumb200s.jpg?ctx=ArtCtxPublic"
+  },
+  {
+    "name": "Dustyn .M WONG",
+    "lifespan": "1993-2011",
+    "mpid": 15558,
+    "thumbSquareUrl": "https://integration.familysearch.org/sandbox/v2/TH-501-45908-27-54/thumb200s.jpg?ctx=ArtCtxPublic"
+  },
+  {
+    "pid": "KWQF-433",
+    "name": "Dustyn .M WONG",
+    "lifespan": "1993-2011",
+    "thumbSquareUrl": "https://integration.familysearch.org/sandbox/v2/TH-501-45908-27-54/thumb200s.jpg?ctx=ArtCtxPublic"
+  }
+]
