14b169b3f3289b8fb6d60c1f6d75b78334ed080e
Chad Eddington <cdavideddington@gmail.com>
2017-04-11 11:15:06 -0600

    Set up travis (#12)

--- wct.conf.js
+++ wct.conf.js
@@ -1,14 +1,12 @@
 module.exports = {
-  plugins: {
-    istanbul: {
-      dir: "./coverage",
-      reporters: [
-        "text-summary", "lcov"
-      ],
-      exclude: [],
-      include: [
-        "**/birch-tree-tagger.html"
-      ],
+  "plugins": {
+    "sauce": {
+      "disabled": true, 
+      "browsers": [
+        "Windows 8.1/internet explorer",
+        "Windows 10/microsoftedge",
+        "OS X 10.10/safari"
+    ]
     }
   }
 }
