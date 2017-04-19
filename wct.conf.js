e5ef974b6b3293a214f14c01f8a9d8e05396ee3d
Chad Eddington <cdavideddington@gmail.com>
2017-04-19 16:14:15 -0600

    Fix tests (#13)
    * Add branch
    
    * Try polyfill
    
    * Fix tests
    
    * Clean up

--- wct.conf.js
+++ wct.conf.js
@@ -5,7 +5,7 @@ module.exports = {
       "browsers": [
         "Windows 8.1/internet explorer",
         "Windows 10/microsoftedge",
-        "OS X 10.10/safari"
+        "OS X 10.12/safari"
     ]
     }
   }
